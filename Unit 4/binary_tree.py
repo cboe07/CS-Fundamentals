b4e7cf0fbd9941f0926c3922eafc65bf6d907a07
Chris Boeckel <cboe07@gmail.com>
2017-06-21 13:09:47 -0400

    Update

---
+++ Unit 4/binary_tree.py
@@ -0,0 +1,30 @@
+class BinaryTreeNode:
+	def __init__(self):
+		self.left_child = None
+		self.right_child = None
+		self.parent= None
+		self.value = value
+
+class MyBinarySearchTree;
+	def __init__(self):
+		self.root = None
+
+	def insert(self,value):
+		new_node = BinaryTreeNode(value)
+
+		if self.root = None:
+			self.root = new_node
+		else:
+			# Traverse the tree and look where to insert the node
+			current_node = self.root
+			while(current_node != None):
+				if current_node.value < new_node.value
+					if self.value > self.right_child
+						return None
+
+		
+
+
+	def exists(self,value):
+		pass
+
